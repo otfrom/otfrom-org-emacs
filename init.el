114b29693c76bc730f74f54711998bb8bee5dec1
Bruce Durling <bld@otfrom.com>
2013-10-20 20:05:43 +0100

    Basics to get org-mode based config going.
    I'd be very happy if someone could come up with a more general and more
    concise way of loading all the files in the org directory. I'm
    reasonably happy with this for now.

---
+++ init.el
@@ -0,0 +1,28 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This is the minimal config needed to get org-mode from melpa and
+;; get it up and running so that we can load our emacs config from a
+;; .org file in a literate manner. The basis for this can be found
+;; here:
+;;
+;; http://orgmode.org/worg/org-contrib/babel/intro.html
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; This means we prefer things from ~/.emacs.d/elpa over the standard packages.
+(package-initialize)
+
+;; This bootstraps us if we don't have anything
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; This installs elpa packages if we haven't done that yet
+(defun maybe-install-and-require (p)
+  (when (not (package-installed-p p))
+    (package-install p))
+  (require p))
+
+(maybe-install-and-require 'org)
+
+(org-babel-load-file (concat (getenv "HOME") "/.emacs.d/org/config.org"))
