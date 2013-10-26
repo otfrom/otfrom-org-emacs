df1bf22f0ef0fe0a864eaeeb335152268be41fd1
Bruce Durling <bld@otfrom.com>
2013-10-26 13:39:33 +0100

    Added gist support.

---
+++ elpa/logito-20120225.2155/logito-autoloads.el
@@ -0,0 +1,18 @@
+;;; logito-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("logito-pkg.el" "logito.el") (21099 46849
+;;;;;;  414021 0))
+
+;;;***
+
+(provide 'logito-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; logito-autoloads.el ends here
---
+++ elpa/pcache-20120408.1206/pcache-autoloads.el
@@ -0,0 +1,18 @@
+;;; pcache-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("pcache-pkg.el" "pcache.el") (21099 46850
+;;;;;;  221902 0))
+
+;;;***
+
+(provide 'pcache-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pcache-autoloads.el ends here
