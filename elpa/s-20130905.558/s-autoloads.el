888e3b6548cf610c35585fee13013eb8d05b126b
Bruce Durling <bld@otfrom.com>
2013-10-28 11:13:52 +0000

    Added tagedit for paredit like html/sgml modes.

---
+++ elpa/s-20130905.558/s-autoloads.el
@@ -0,0 +1,18 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21102 18042 139636
+;;;;;;  0))
+
+;;;***
+
+(provide 's-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-autoloads.el ends here
---
+++ elpa/tagedit-20130816.1410/tagedit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tagedit" "20130816.1410" "Some paredit-like features for html-mode [github]" (quote ((s "1.3.1") (dash "1.0.3"))))
