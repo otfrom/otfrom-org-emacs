4e3d3f7606b9db38ea7279f91de510ec6b9a4d69
Bruce Durling <bld@otfrom.com>
2013-11-02 17:48:04 +0000

    elpa update.

--- elpa/git-commit-mode-20131029.1425/git-commit-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; git-commit-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (git-commit-mode) "git-commit-mode" "git-commit-mode.el"
-;;;;;;  (21105 15308 0 0))
-;;; Generated autoloads from git-commit-mode.el
-
-(autoload 'git-commit-mode "git-commit-mode" "\
-Major mode for editing git commit messages.
-
-This mode helps with editing git commit messages both by
-providing commands to do common tasks, and by highlighting the
-basic structure of and errors in git commit messages.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("git-commit-mode-pkg.el") (21105 15308
-;;;;;;  792440 0))
-
-;;;***
-
-(provide 'git-commit-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; git-commit-mode-autoloads.el ends here
