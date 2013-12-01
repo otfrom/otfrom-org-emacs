086dfda825f2925161333496f62d7c2d58778a9c
Bruce Durling <bld@otfrom.com>
2013-12-01 22:32:28 +0000

    Added elnode.

---
+++ elpa/elnode-20130416.1626/elnode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "elnode" "20130416.1626" "The Emacs webserver."
+  '((web "0.1.4")
+    (creole "0.8.14")
+    (fakir "0.0.14")
+    (db "0.0.5")
+    (kv "0.0.15")))
---
+++ elpa/noflet-20130901.1722/noflet-autoloads.el
@@ -0,0 +1,18 @@
+;;; noflet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("noflet-pkg.el" "noflet.el") (21147 46459
+;;;;;;  914779 0))
+
+;;;***
+
+(provide 'noflet-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; noflet-autoloads.el ends here
