2a67704f74432a9e99296a8a2cae64ff0e3d4505
Bruce Durling <bld@otfrom.com>
2013-12-16 10:50:39 +0000

    MELPA only for now.
    Skipping marmalade for now. I'm sure it will come back.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ;; ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; This means we prefer things from ~/.emacs.d/elpa over the standard packages.
