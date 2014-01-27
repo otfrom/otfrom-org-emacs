1dd751602efeb43e87e6bd2690d028a389c47f40
Bruce Durling <bld@otfrom.com>
2014-01-27 00:13:17 +0000

    Added org-magit.

---
+++ elpa/org-magit-20120820.1156/org-magit-autoloads.el
@@ -0,0 +1,41 @@
+;;; org-magit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (org-magit-store-link org-magit-export org-magit-open)
+;;;;;;  "org-magit" "org-magit.el" (21221 42128 0 0))
+;;; Generated autoloads from org-magit.el
+
+(autoload 'org-magit-open "org-magit" "\
+
+
+\(fn STR)" nil nil)
+
+(autoload 'org-magit-export "org-magit" "\
+
+
+\(fn PATH DESC FORMAT)" nil nil)
+
+(autoload 'org-magit-store-link "org-magit" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load "org" '(progn (org-add-link-type "magit" 'org-magit-open 'org-magit-export) (add-hook 'org-store-link-functions 'org-magit-store-link)))
+
+;;;***
+
+;;;### (autoloads nil nil ("org-magit-pkg.el") (21221 42128 328713
+;;;;;;  0))
+
+;;;***
+
+(provide 'org-magit-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; org-magit-autoloads.el ends here
