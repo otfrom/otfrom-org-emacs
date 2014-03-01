1bc1d09f8a561b24f75994b2514ff765cd3bf479
Bruce Durling <bld@rosetta.(none)>
2014-03-01 10:57:30 +0000

    Added org-pomodoro

---
+++ elpa/alert-20130510.20/alert-pkg.el
@@ -0,0 +1,1 @@
+(define-package "alert" "20130510.20" "Growl-style notification system for Emacs" (quote nil))
---
+++ elpa/org-pomodoro-20131117.35/org-pomodoro-pkg.el
@@ -0,0 +1,2 @@
+(define-package "org-pomodoro" "20131117.35" "Pomodoro implementation for org-mode."
+  '((alert "0.5.10")))
