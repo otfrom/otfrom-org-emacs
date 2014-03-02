e2181e1d41aa9e448c6f4c79f7bd2e26bda3070f
Bruce Durling <bld@rosetta.(none)>
2014-03-02 12:06:29 +0000

    Dump bbdb. We're using org-contacts now.

--- elpa/bbdb-20140228.2021/bbdb-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bbdb" "20140228.2021" "The Insidious Big Brother Database for GNU Emacs" 'nil)
--- elpa/bbdb2erc-20130606.2329/bbdb2erc-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bbdb2erc" "20130606.2329" "make bbdb show if pal is online with ERC, click i to chat" (quote ((bbdb "3.0"))))
