3bb19a27cf627fc965c9b08ed5337eb8f9397039
Bruce Durling <bruce@mastodonc.com>
2014-03-09 13:37:48 +0000

    Added ac-helm for better autocompletion.

---
+++ elpa/ac-helm-20131224.647/ac-helm-autoloads.el
@@ -0,0 +1,30 @@
+;;; ac-helm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ac-complete-with-helm) "ac-helm" "ac-helm.el"
+;;;;;;  (21276 28017 220532 195000))
+;;; Generated autoloads from ac-helm.el
+
+(autoload 'ac-complete-with-helm "ac-helm" "\
+Select `auto-complete' candidates by `helm'.
+It is useful to narrow candidates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-helm-pkg.el") (21276 28017 252426
+;;;;;;  140000))
+
+;;;***
+
+(provide 'ac-helm-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-helm-autoloads.el ends here
