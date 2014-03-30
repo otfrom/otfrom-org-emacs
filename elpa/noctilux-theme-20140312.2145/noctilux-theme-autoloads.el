b6a9b30b83afa05eb52c1de1fbd64df09568ee52
Bruce Durling <bruce@mastodonc.com>
2014-03-30 17:04:22 +0100

    Trying noctilux.

---
+++ elpa/noctilux-theme-20140312.2145/noctilux-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; noctilux-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "noctilux-definitions" "noctilux-definitions.el"
+;;;;;;  (21282 10686 898757 241000))
+;;; Generated autoloads from noctilux-definitions.el
+
+(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("noctilux-theme-pkg.el" "noctilux-theme.el")
+;;;;;;  (21282 10686 916085 254000))
+
+;;;***
+
+(provide 'noctilux-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; noctilux-theme-autoloads.el ends here
