c410df480f58370cacb44c267550558bfc43586e
Bruce Durling <bruce@mastodonc.com>
2014-05-18 21:22:36 +0100

    Move from autocomplete to company.
    Company is very very good. I'm impressed so far.

--- elpa/ac-helm-20131224.647/ac-helm-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; ac-helm-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (ac-complete-with-helm) "ac-helm" "ac-helm.el"
-;;;;;;  (21348 61937 483178 949000))
-;;; Generated autoloads from ac-helm.el
-
-(autoload 'ac-complete-with-helm "ac-helm" "\
-Select `auto-complete' candidates by `helm'.
-It is useful to narrow candidates.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ac-helm-pkg.el") (21348 61937 500046
-;;;;;;  508000))
-
-;;;***
-
-(provide 'ac-helm-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ac-helm-autoloads.el ends here
--- elpa/ac-helm-20131224.647/ac-helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ac-helm" "20131224.647" "Helm interface for auto-complete" (quote ((helm "20130328") (auto-complete "1.4.0") (popup "0.5.0"))))
