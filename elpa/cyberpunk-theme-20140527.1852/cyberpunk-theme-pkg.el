6c6f777d63d725d60715edf5d2575df5a9379986
Bruce Durling <bruce@mastodonc.com>
2014-07-09 23:28:57 +0100

    elpa update.

--- elpa/cyberpunk-theme-20140527.1852/cyberpunk-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cyberpunk-theme" "20140527.1852" "Cyberpunk Color Theme" (quote nil))
--- elpa/emms-20140529.1117/emms-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "emms" "20140529.1117" "The Emacs Multimedia System" 'nil :keywords
-  '("emms" "mp3" "mpeg" "multimedia"))
